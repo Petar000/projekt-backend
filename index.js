23a74e019c5f4f95c3867102e1b2ac3eaa07d7cc
pevukovic <pvukovic1111@gmail.com>
2023-07-28 13:29:28 +0200

    server

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.delete("/izbrisisve", (req, res) => {
     }
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
